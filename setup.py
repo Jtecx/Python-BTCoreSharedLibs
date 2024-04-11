587f862fb3b65461a97cfb4227f9a81d587eb1e6
Jtex <58648760+Jtecx@users.noreply.github.com>
2024-04-11 02:01:18 -0700

    Package of commonly reused functions across scripts.
    Package of commonly reused functions across scripts, so I don't have to keep rewriting the same damn things every time.

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="BTCoreSharedLibs",
+    version="0.0.2",
+    python_requires=">=3.10",
+    packages=["CoreSharedLibs"],
+    install_requires=["numpy"],
+)
