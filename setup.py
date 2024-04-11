f7da343b26634c7e30e9e18b89d0aa66d39dc936
Jtex <58648760+Jtecx@users.noreply.github.com>
2024-04-11 03:27:38 -0700

    Attempt at setting up github push to pypi packages.

--- setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name="BTCoreSharedLibs",
-    version="0.0.2",
-    python_requires=">=3.10",
-    packages=["CoreSharedLibs"],
-    install_requires=["numpy"],
-)
